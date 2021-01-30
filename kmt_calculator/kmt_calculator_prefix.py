a01b8578b823a326b90e7731a02d2b79f85f83d6
BKlasmer <BenKlasmer@Benjamins-MacBook-Pro.local>
2021-01-30 13:12:18 +0000

    Initial commit

---
+++ kmt_calculator/kmt_calculator_prefix.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+# -*- coding: latin-1 -*-
+
+""" Calculator to process prefix notation (e.g + * 1 2 3)
+"""
+
+class CalculatorPrefix(object):
+    def __init__(self):
+        pass
