a3a362f98ab04b24745ceedfaafb1d6fd7c12806
Ben Klasmer <benjamin_aram@hotmail.com>
2021-01-30 13:49:14 +0000

    Add stack and operands to init

--- kmt_calculator/kmt_calculator_prefix.py
+++ kmt_calculator/kmt_calculator_prefix.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # -*- coding: latin-1 -*-
 
+import operator
 from kmt_calculator.utils import Logging
 
 """ Calculator to process prefix notation (e.g + * 1 2 3)
@@ -10,3 +11,10 @@ class CalculatorPrefix(object):
     def __init__(self, logger_level: str = "INFO") -> None:
         self._logger = Logging().create_logger(logger_name="CalculatorPrefix", logger_level=logger_level)
         self._logger.info("Initialise Prefix Calculator")
+        self._stack = []
+        self._operands = {
+            "+": operator.add,
+            "-": operator.sub,
+            "*": operator.mul,
+            "/": operator.floordiv,
+        }
