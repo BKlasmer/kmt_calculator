31c0162c94a11714182249529a7c551c02dd1c7e
Ben Klasmer <benjamin_aram@hotmail.com>
2021-01-30 13:42:12 +0000

    Initialise the logger

--- kmt_calculator/kmt_calculator_prefix.py
+++ kmt_calculator/kmt_calculator_prefix.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 # -*- coding: latin-1 -*-
 
+from kmt_calculator.utils import Logging
+
 """ Calculator to process prefix notation (e.g + * 1 2 3)
 """
 
 class CalculatorPrefix(object):
-    def __init__(self):
-        pass
+    def __init__(self, logger_level: str = "INFO") -> None:
+        self._logger = Logging().create_logger(logger_name="CalculatorPrefix", logger_level=logger_level)
+        self._logger.info("Initialise Prefix Calculator")
