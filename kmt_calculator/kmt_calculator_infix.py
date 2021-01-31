41c33faedd4987f2433031249ec8f12230d532f5
Ben Klasmer <benjamin_aram@hotmail.com>
2021-01-31 11:42:33 +0000

    Boilerplate infix to prefix conversion method

--- kmt_calculator/kmt_calculator_infix.py
+++ kmt_calculator/kmt_calculator_infix.py
@@ -14,9 +14,23 @@ class CalculatorInfix(CalculatorPrefix):
         self._logger = Logging().create_logger(logger_name="CalculatorInfix", logger_level=logger_level)
         self._logger.info("Initialise Infix Calculator")
         self._stack = []
+        self._operator_stack = []
         self._operators = {
             "+": operator.add,
             "-": operator.sub,
             "*": operator.mul,
             "/": operator.floordiv,
         }
+
+    def convert_infix_to_prefix(self, infix_expression: str) -> str:
+
+        for element in infix_expression.split(" "):
+            print(element)
+            if element == "(":
+                self._operator_stack.append(element)
+            elif element == ")":
+                pass # Add logic later
+            elif element in self._operators:
+                self._operator_stack.append(element)
+            else:
+                self._stack.append(element)
